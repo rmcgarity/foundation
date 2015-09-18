e416a9353a8c51fac35c231177bba4dec726a5d9
rmcgarity <ralph_mcgarity@yahoo.com>
2015-09-18 10:55:48 -0500

    Completed jQuery-Collect-Album Checkpoint

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,39 +1,33 @@
-var collectionItemTemplate = 
-    '<div class="collection-album-container column fourth">'
-  + '  <img src="assets/images/album_covers/01.png"/>'
-  + '  <div class="collection-album-info caption">'
-  + '    <p>'
-  + '      <a class="album-name" href="/album.html"> The Colors </a>'
-  + '      <br/>'
-  + '      <a href="/album.html"> Pablo Picasso </a>'
-  + '      <br/>'
-  + '      X songs'
-  + '      <br/>'
-  + '    </p>'
-  + '  </div>'
-  + '</div>';
+var buildCollectionItemTemplate = function() {
 
-function getAllProperties(obj) {
-  var properties = '';
-  for (property in obj) {
-    properties += '\n' + property;
-  }
-  alert('Properties of object:' + properties);
-}
+    var template =
+        '<div class="collection-album-container column fourth">'
+      + '  <img src="assets/images/album_covers/01.png"/>'
+      + '  <div class="collection-album-info caption">'
+      + '    <p>'
+      + '      <a class="album-name" href="/album.html"> The Colors </a>'
+      + '      <br/>'
+      + '      <a href="/album.html"> Pablo Picasso </a>'
+      + '      <br/>'
+      + '      X songs'
+      + '      <br/>'
+      + '    </p>'
+      + '  </div>'
+      + '</div>';
+    return $(template);
+};
 
-window.onload = function() {
+$(window).load(function() {
 
-    // #1
-    //var collectionContainer = document.getElementsByName('ralph')[0].childNodes[1];
-    var collectionContainer = document.getElementsByClassName('album-covers')[0].childNodes[1];
+    // Assign the album container that has the .clearfix classes on it
+    var $collectionContainer = $('.album-covers.clearfix');
     
-    // # 2
-    collectionContainer.innerHTML = '';
+    // Clear the contents of the HTML just in case something else has been dynamically inserted
+    $collectionContainer.empty();
     
-    // getAllProperties(collectionContainer);
-    // #3
+    // Insert all of the albums in the collection containers
     for (var i = 0; i < 12; i++) {
-       collectionContainer.innerHTML += collectionItemTemplate;
+        var $newThumbnail = buildCollectionItemTemplate();
+        $collectionContainer.append($newThumbnail);
     }
-    
-}
+});
